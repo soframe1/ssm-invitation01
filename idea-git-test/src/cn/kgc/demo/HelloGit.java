814aba912e8ba383b006514f04eb8e57bc893add
赵劲 <479952026@qq.com>
2021-05-22 23:02:49 +0800

    添加test工程到git

---
+++ idea-git-test/src/cn/kgc/demo/HelloGit.java
@@ -0,0 +1,15 @@
+package cn.kgc.demo;
+
+/**
+ * @ClassName HelloGit
+ * @Description TODO
+ * @Author zhaojing
+ * @Date 2021/5/22 22:56
+ * @Version 1.0
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!");
+    }
+}
